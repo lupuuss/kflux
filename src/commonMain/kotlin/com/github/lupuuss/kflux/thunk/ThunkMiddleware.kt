d7940c5ed7aa33b0cae85b1f32134083e617949e
lupuuss <damian.baczynski@gmail.com>
2022-02-24 20:20:14 +0100

    Add explicit name to ThunkMiddleware.kt

--- src/commonMain/kotlin/com/github/lupuuss/kflux/thunk/ThunkMiddleware.kt
+++ src/commonMain/kotlin/com/github/lupuuss/kflux/thunk/ThunkMiddleware.kt
@@ -4,11 +4,11 @@ import com.github.lupuuss.kflux.core.context.element.coroutineScope
 import com.github.lupuuss.kflux.core.middleware.consumingMiddleware
 import kotlinx.coroutines.launch
 
-fun <State : Any> thunkMiddleware() = consumingMiddleware<Thunk, State> {
-    when (it) {
-        is Thunk.CoreExecutable -> with(it) { execute() }
-        is Thunk.CoreSuspendable -> coroutineScope.launch(it.coroutineContext, it.coroutineStart) {
-            with(it) { executeSuspendable() }
+fun <State : Any> thunkMiddleware() = consumingMiddleware<Thunk, State> { thunk ->
+    when (thunk) {
+        is Thunk.CoreExecutable -> with(thunk) { execute() }
+        is Thunk.CoreSuspendable -> coroutineScope.launch(thunk.coroutineContext, thunk.coroutineStart) {
+            with(thunk) { executeSuspendable() }
         }
     }
 }
