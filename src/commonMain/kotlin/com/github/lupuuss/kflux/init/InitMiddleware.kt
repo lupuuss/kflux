6c61a727624b96c5d6130e5a71cce96401218808
Damian Baczyński <damian.baczynski@daftmobile.com>
2022-02-27 20:09:15 +0100

    Pass first action to actual middleware in InitMiddleware.kt

--- src/commonMain/kotlin/com/github/lupuuss/kflux/init/InitMiddleware.kt
+++ src/commonMain/kotlin/com/github/lupuuss/kflux/init/InitMiddleware.kt
@@ -3,11 +3,13 @@ package com.github.lupuuss.kflux.init
 import com.github.lupuuss.kflux.core.middleware.Middleware
 import com.github.lupuuss.kflux.core.middleware.translucentMiddleware
 import com.github.lupuuss.kflux.core.scope.DispatchScope
+import kotlin.experimental.ExperimentalTypeInference
 
-fun <State : Any> initMiddleware(block: DispatchScope<State>.() -> Middleware<State>): Middleware<State> {
+@OptIn(ExperimentalTypeInference::class)
+fun <State : Any> initMiddleware(@BuilderInference block: DispatchScope<State>.() -> Middleware<State>): Middleware<State> {
     var currentMiddleware: Middleware<State>
     val initMiddleware = translucentMiddleware<State> {
-        currentMiddleware = block()
+        currentMiddleware = block().apply { process(it) }
     }
     currentMiddleware = initMiddleware
     return Middleware { currentMiddleware.run { process(it) } }
