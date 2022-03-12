4f69e084305ba404909bb89e1720748f314da90f
lupuuss <damian.baczynski@gmail.com>
2022-03-12 23:59:52 +0100

    Add CustomMiddleware.kt to core example

---
+++ src/commonTest/kotlin/com/github/lupuuss/kflux/CustomMiddleware.kt
@@ -0,0 +1,27 @@
+package com.github.lupuuss.kflux
+
+import com.github.lupuuss.kflux.core.Action
+import com.github.lupuuss.kflux.core.middleware.Middleware
+import com.github.lupuuss.kflux.core.middleware.consume
+import com.github.lupuuss.kflux.core.middleware.pass
+
+private sealed class CustomAction : Action {
+
+    object A : CustomAction()
+    object B : CustomAction()
+    object C : CustomAction()
+}
+
+private fun customMiddleware() = Middleware<AppState> { action ->
+    when (action) {
+        is CustomAction.A, is CustomAction.B -> {
+            println("PASS $action")
+            pass()
+        }
+        is CustomAction.C -> {
+            println("CONSUME: $action")
+            consume()
+        }
+        else -> pass()
+    }
+}
