998d9857507003cf9a532ba4c0e890674e3cea9e
lupuuss <damian.baczynski@gmail.com>
2022-03-13 16:42:16 +0100

    Make TaskDataSource useful in tests

--- src/commonTest/kotlin/com/github/lupuuss/kflux/data/TaskDataSource.kt
+++ src/commonTest/kotlin/com/github/lupuuss/kflux/data/TaskDataSource.kt
@@ -4,10 +4,16 @@ import com.github.lupuuss.kflux.task.Task
 import kotlinx.coroutines.delay
 import kotlinx.datetime.LocalDateTime
 
-class TaskDataSource : DataSource<Unit, List<Task>> {
+class TaskDataSource(
+    private val tasks: List<Task> = default
+) : DataSource<Unit, List<Task>> {
     override suspend fun get(request: Unit): List<Task> {
         delay(1000)
-        return listOf(
+        return tasks
+    }
+
+    companion object {
+        private val default = listOf(
             Task("1", "Task 1", false, LocalDateTime(2021, 1, 1, 1, 1)),
             Task("2", "Task 2", false, LocalDateTime(2021, 1, 1, 1, 1)),
             Task("3", "Task 3", false, LocalDateTime(2021, 1, 1, 1, 1)),
