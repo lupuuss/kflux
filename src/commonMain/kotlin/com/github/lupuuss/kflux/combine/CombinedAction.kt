5d74fd6f2e139de4af9ed28010421c7866a27925
lupuuss <damian.baczynski@gmail.com>
2022-03-13 16:40:16 +0100

    Make CombinedAction.kt a data class

--- src/commonMain/kotlin/com/github/lupuuss/kflux/combine/CombinedAction.kt
+++ src/commonMain/kotlin/com/github/lupuuss/kflux/combine/CombinedAction.kt
@@ -4,6 +4,7 @@ import com.github.lupuuss.kflux.core.Action
 import com.github.lupuuss.kflux.core.scope.DispatchScope
 import com.github.lupuuss.kflux.thunk.Thunk
 
-class CombinedAction(val actions: List<Action>) : Thunk.Executable<Nothing> {
+data class CombinedAction(val actions: List<Action>) : Thunk.Executable<Nothing> {
+
     override fun DispatchScope<Nothing>.execute() = actions.forEach(this::dispatch)
 }
