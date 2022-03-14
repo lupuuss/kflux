77950d38cf3df4ddb5721d328c2e3d6df407c793
lupuuss <damian.baczynski@gmail.com>
2022-03-15 00:13:44 +0100

    Simplify AppStoreTest.kt

--- src/commonTest/kotlin/com/github/lupuuss/kflux/AppStoreTest.kt
+++ src/commonTest/kotlin/com/github/lupuuss/kflux/AppStoreTest.kt
@@ -4,6 +4,7 @@ import com.github.lupuuss.kflux.task.TaskAction
 import com.github.lupuuss.kflux.thunk.thunkSuspend
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
@@ -14,8 +15,8 @@ class AppStoreTest {
     fun test() = runTest {
         val store = appStore(this)
         store.dispatch(TaskAction.Load)
+        advanceUntilIdle()
         store.dispatch(thunkSuspend<AppState> {
-            delay(2000)
             println("-----------------")
             println(state)
         })
