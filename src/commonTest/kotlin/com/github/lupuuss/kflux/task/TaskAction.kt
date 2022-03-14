234b4d5bffffd3e0004ea8a0f24f11e83aa50b29
lupuuss <damian.baczynski@gmail.com>
2022-03-15 00:21:03 +0100

    Fix after rebase

--- src/commonTest/kotlin/com/github/lupuuss/kflux/task/TaskAction.kt
+++ src/commonTest/kotlin/com/github/lupuuss/kflux/task/TaskAction.kt
@@ -2,30 +2,27 @@ package com.github.lupuuss.kflux.task
 
 import com.github.lupuuss.kflux.AppState
 import com.github.lupuuss.kflux.DateProvider
-import com.github.lupuuss.kflux.core.scope.DispatchScope
+import com.github.lupuuss.kflux.combine.plus
+import com.github.lupuuss.kflux.core.Action
+import com.github.lupuuss.kflux.data.DataSourceFactory
+import com.github.lupuuss.kflux.data.DataSourceLoader
 import com.github.lupuuss.kflux.kodein.di
 import com.github.lupuuss.kflux.thunk.Thunk
-import kotlinx.datetime.LocalDateTime
 import org.kodein.di.instance
 
 sealed class TaskAction : Action {
 
-    data class Complete(val id: String) : Thunk.Executable<AppState> {
-        override fun DispatchScope<AppState>.execute() {
-            val dateProvider by di.instance<DateProvider>()
-            val newTask = state.getTaskById(id).copy(isDone = true, modificationDate = dateProvider.get())
-            dispatch(TaskStateAction.Replace(newTask))
-        }
-    }
+    data class Complete(val id: String) : Thunk.Execute<AppState>({
+        val dateProvider by di.instance<DateProvider>()
+        val newTask = state.getTaskById(id).copy(isDone = true, modificationDate = dateProvider.get())
+        dispatch(TaskStateAction.Replace(newTask))
+    })
 
-    object Load : Thunk.Executable<AppState> {
-        override fun DispatchScope<AppState>.execute() {
-            val tasks = listOf(
-                Task("1", "Task 1", false, LocalDateTime(2021, 1, 1, 1, 1)),
-                Task("2", "Task 2", false, LocalDateTime(2021, 1, 1, 1, 1)),
-                Task("3", "Task 3", false, LocalDateTime(2021, 1, 1, 1, 1)),
-            )
-            dispatch(TaskStateAction.SetAll(tasks))
-        }
-    }
+    object Load : DataSourceLoader<Unit, List<Task>>(
+        request = Unit,
+        dataSource = DataSourceFactory::tasks,
+        onSuccess = { TaskStateAction.SetAll(it) + Complete(it.first().id) },
+    )
 }
+
+private fun AppState.getTaskById(id: String) = tasks.first { it.id == id }
