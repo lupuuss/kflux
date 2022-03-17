da478155fd1e7eb0c733d5397863a3c8951b57d0
lupuuss <damian.baczynski@gmail.com>
2022-03-17 19:47:39 +0100

    Add flavor check utils

--- src/commonMain/kotlin/com/github/lupuuss/kflux/flavor/FlavoredStoreBuilder.kt
+++ src/commonMain/kotlin/com/github/lupuuss/kflux/flavor/FlavoredStoreBuilder.kt
@@ -3,6 +3,9 @@ package com.github.lupuuss.kflux.flavor
 import com.github.lupuuss.kflux.core.store.builder.*
 import kotlin.experimental.ExperimentalTypeInference
 
+@DslMarker
+annotation class FlavorBuilderDslMarker
+
 @OptIn(ExperimentalTypeInference::class)
 fun <Flavor : StoreFlavor, State> buildFlavoredStore(
     flavor: Flavor,
@@ -14,6 +17,19 @@ fun <Flavor : StoreFlavor, State> buildFlavoredStore(
 
 interface FlavoredStoreBuilderScope<Flavor : StoreFlavor, State> : StoreBuilderScope<State> {
     val flavor: Flavor
+}
+
+@FlavorBuilderDslMarker
+inline fun <reified T : StoreFlavor> FlavoredStoreBuilderScope<*, *>.on(block: (T) -> Unit) {
+    (flavor as? T)?.let(block)
+}
+
+@FlavorBuilderDslMarker
+inline fun <reified T : StoreFlavor> FlavoredStoreBuilderScope<*, *>.on(flavor: T, block: (T) -> Unit) {
+    this.flavor
+        .takeIf { it == flavor }
+        .let { it as? T }
+        ?.let(block)
 }
 
 internal class FlavoredStoreBuilder<Flavor : StoreFlavor, State>(
