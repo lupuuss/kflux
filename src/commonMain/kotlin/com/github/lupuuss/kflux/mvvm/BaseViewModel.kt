aea4174675cc5090d3625d30e300a6d4f856a06f
Damian Baczyński <damian.baczynski@daftmobile.com>
2022-03-19 20:49:03 +0100

    Add BaseViewModel.kt

---
+++ src/commonMain/kotlin/com/github/lupuuss/kflux/mvvm/BaseViewModel.kt
@@ -0,0 +1,25 @@
+package com.github.lupuuss.kflux.mvvm
+
+import com.github.lupuuss.kflux.core.store.Store
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.*
+
+abstract class BaseViewModel<State>(
+    protected val store: Store<State>,
+    protected val coroutineScope: CoroutineScope
+) {
+    protected fun <T, R> StateFlow<T>.mapState(transform: (T) -> R): StateFlow<R> = map(transform)
+        .stateInHere(initialValue = transform(value))
+
+    protected fun <T> Flow<T>.stateInHere(
+        started: SharingStarted = SharingStarted.Lazily,
+        initialValue: T
+    ) = stateIn(coroutineScope, started, initialValue)
+
+    protected fun <T> Flow<T>.stateInHere(started: SharingStarted = SharingStarted.Lazily) =
+        stateIn(coroutineScope, started, null)
+
+    protected fun <T> Flow<T>.common() = CFlow(this, coroutineScope)
+
+    protected fun <T> StateFlow<T>.common() = CStateFlow(this, coroutineScope)
+}
